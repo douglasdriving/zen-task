81f89a57d91db1feac019a6fd64bf8d143dfb526
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-10-05 18:19:30 +0900

    retrieval bug fixes

--- scripts/task_retriever.py
+++ scripts/task_retriever.py
@@ -20,26 +20,6 @@ class TaskRetriever:
         tasks = self._make_tasks_from_data(tasks_data)
         return tasks
 
-    def _make_tasks_from_data(self, tasks_data):
-        tasks: list[Task] = []
-        for task_data in tasks_data:
-            task = Task(
-                description=task_data[1],
-                detailed_steps=task_data[2],
-                definition_of_done=task_data[3],
-                deadline=datetime.fromtimestamp(task_data[4]),
-                project=task_data[5],
-            )
-            task.rate(
-                value=task_data[6],
-                excitement=task_data[7],
-                estimated_time_in_minutes=task_data[8],
-                cognitive_load=task_data[9],
-            )
-            task.id = task_data[0]
-            tasks.append(task)
-        return tasks
-
     def _get_available_tasks_from_db(self):
         db = sqlite3.connect("tasks.db")
         cursor = db.cursor()
@@ -54,6 +34,37 @@ class TaskRetriever:
         tasks_data = cursor.fetchall()
         db.close()
         return tasks_data
+
+    def _make_tasks_from_data(self, tasks_data):
+        tasks: list[Task] = []
+        for task_data in tasks_data:
+            task: Task = self._make_task_from_data(task_data)
+            tasks.append(task)
+        return tasks
+
+    def _make_task_from_data(self, task_data):
+        deadline = None
+        if task_data[4] != None:
+            deadline = datetime.fromtimestamp(task_data[4])
+        waiting_until = None
+        if task_data[11] != None:
+            waiting_until = datetime.fromtimestamp(task_data[11])
+        task = Task(
+            description=task_data[1],
+            detailed_steps=task_data[2],
+            definition_of_done=task_data[3],
+            deadline=deadline,
+            waiting_until=waiting_until,
+            project=task_data[5],
+        )
+        task.rate(
+            value=task_data[6],
+            excitement=task_data[7],
+            estimated_time_in_minutes=task_data[8],
+            cognitive_load=task_data[9],
+        )
+        task.id = task_data[0]
+        return task
 
     def _pick_best_task(self, tasks: list[Task]):
         next_task: Task = tasks[0]
